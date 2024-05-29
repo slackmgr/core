f5aa21363f2b3a47a3aac48887eb69026a794334
Peter Aglen <peteraglen@gmail.com>
2024-05-29 20:39:56 +0200

    wip

--- api/handle_mappings.go
+++ api/handle_mappings.go
@@ -10,9 +10,9 @@ import (
 func (s *Server) handleMappings(resp http.ResponseWriter, req *http.Request) {
 	started := time.Now()
 
-	data, err := json.MarshalIndent(s.apiSettings, "", "  ")
+	data, err := json.MarshalIndent(s.apiSettings.RoutingRules, "", "  ")
 	if err != nil {
-		err = fmt.Errorf("failed to marshal alert mappings: %w", err)
+		err = fmt.Errorf("failed to json marshal API routing rules: %w", err)
 		s.writeErrorResponse(req.Context(), err, http.StatusInternalServerError, nil, "", resp, req, started)
 		return
 	}
@@ -21,7 +21,7 @@ func (s *Server) handleMappings(resp http.ResponseWriter, req *http.Request) {
 	resp.WriteHeader(http.StatusOK)
 
 	if _, err = resp.Write(data); err != nil {
-		s.logger.Errorf("Failed to write mappings response: %s", err)
+		s.logger.Errorf("Failed to write API routing rules response: %s", err)
 	}
 
 	s.metrics.AddHTTPRequestMetric(req.URL.Path, req.Method, http.StatusOK, time.Since(started))
